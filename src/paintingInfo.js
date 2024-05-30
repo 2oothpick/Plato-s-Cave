367f5475d5ef1ba2c34646795b865e6c7e9995fa
2oothpick <kobbyboateng14@gmail.com>
2024-05-30 23:00:16 +0000

    Refactor code Separate code into modules

---
+++ src/paintingInfo.js
@@ -0,0 +1,16 @@
+// paintingInfo.js
+export function displayPaintingInfo(info) {
+  const infoElement = document.getElementById('painting-info');
+  infoElement.innerHTML = `
+    <h3>${info.title}</h3>
+    <p>Artist: ${info.artist}</p>
+    <p>Description: ${info.description}</p>
+    <p>Year: ${info.year}</p>
+  `;
+  infoElement.classList.add('show');
+}
+
+export function hidePaintingInfo() {
+  const infoElement = document.getElementById('painting-info');
+  infoElement.classList.remove('show');
+}
